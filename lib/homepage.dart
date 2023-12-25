c81ecd7e22e6151b86a059d482964418b5ecf881
Kartikey Srivastava <kartikeyai246@gmail.com>
2023-12-25 21:43:42 +0530

    base structure

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Center(child: Text('Mask')),
+      ),
+      body: const Center(
+        child: Text('Mask Detection App'),
+      ),
+    );
+  }
+}
